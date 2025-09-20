5c649d01c8d89e07fd538f5dc6ba46a8d4b9f60d
Sreeraj S Kurup <154326486+s-r-e-e-r-a-j@users.noreply.github.com>
2025-09-20 14:50:19 +0530

    Update script.js

--- Language Translator/script.js
+++ Language Translator/script.js
@@ -1,75 +1,79 @@
-    //coded by sreeraj
-   var writingarea=document.querySelector("#writingarea");
-  var outputarea=document.querySelector("#outputarea");
-  var inputlang=document.querySelector("#inputlang");
-  var outputlang=document.querySelector("#outputlang");
-  var translatebtn=document.querySelector("#translatebtn").addEventListener("click",translate);
-  var spkbtn=document.querySelector("#spkbtn").addEventListener("click",speak)
- 
+// coded by Sreeraj
+const writingarea = document.querySelector("#writingarea");
+const outputarea = document.querySelector("#outputarea");
+const inputlang = document.querySelector("#inputlang");
+const outputlang = document.querySelector("#outputlang");
 
+document.querySelector("#translatebtn").addEventListener("click", translate);
+document.querySelector("#spkbtn").addEventListener("click", speak);
 
-  const languages=[
-  {name:"Malayalam",code:"ml-IN"},{name:"English",code:"en-US"},{name:"Hindi",code:"hi-IN"},{name:"Tamil",code:"ta-IN"},{name:"Arabic",code:"ar-XA"},{name:"Chinese",code:"yue-HK"},{name:"Danish",code:"da-DK"},
-    {name:"French",code:"fr-CA"},{name:"German",code:"de-DE"},{name:"Japanese",code:"ja-JP"},
-    {name:"Korean",code:"ko-KR"},{name:"spanish",code:"es-US"},{name:"Telungu",code:"te-IN"}
-    ]
-    
-  
-  
+const languages = [
+  { name: "Malayalam", code: "ml-IN" },
+  { name: "English", code: "en-US" },
+  { name: "Hindi", code: "hi-IN" },
+  { name: "Tamil", code: "ta-IN" },
+  { name: "Arabic", code: "ar-XA" },
+  { name: "Chinese", code: "yue-HK" },
+  { name: "Danish", code: "da-DK" },
+  { name: "French", code: "fr-CA" },
+  { name: "German", code: "de-DE" },
+  { name: "Japanese", code: "ja-JP" },
+  { name: "Korean", code: "ko-KR" },
+  { name: "Spanish", code: "es-US" },
+  { name: "Telugu", code: "te-IN" }
+];
 
- 
+// Populate languages in dropdowns
+languages.forEach(language => {
+  const option1 = document.createElement("option");
+  option1.text = language.name;
+  option1.value = language.code;
+  inputlang.appendChild(option1);
 
-  languages.forEach((language)=>{
+  const option2 = document.createElement("option");
+  option2.text = language.name;
+  option2.value = language.code;
+  outputlang.appendChild(option2);
+});
 
-    //Options for inputlang(choose writing language)
-    var options1=document.createElement("option");
-    options1.text=language.name;
-    options1.value=language.code;
-    inputlang.appendChild(options1);
+let writinglanguage, convertinglanguage, speaktext;
 
-    //Options for outputlang(choose converting language)
-    var options2=document.createElement('option');
-    options2.text=language.name;
-    options2.value=language.code;
-    outputlang.appendChild(options2);
+inputlang.addEventListener("change", () => {
+  writinglanguage = inputlang.value;
+});
 
-  })
-  var writinglanguage
-  var convertinglanguage
-   inputlang.addEventListener("change",()=>{
-    writinglanguage=inputlang.value;
-  })
-  outputlang.addEventListener("change",()=>{
-    convertinglanguage=outputlang.value;
-  })
-  
- var speaktext
+outputlang.addEventListener("change", () => {
+  convertinglanguage = outputlang.value;
+});
 
-        function translate(){
-           var text=writingarea.value;
-     var api=`https://api.mymemory.translated.net/get?q=${text}&langpair=${writinglanguage}|${convertinglanguage}`
-    fetch(api).then((response)=>response.json()).then((data)=>{
-      outputarea.innerHTML=data.responseData.translatedText;
-      speaktext=data.responseData.translatedText
-  
-    }).catch((error)=>{
-      outputarea.innerHTML="Error Occured"
+function translate() {
+  const text = writingarea.value.trim();
+  if (!text) {
+    outputarea.innerHTML = "Please enter some text.";
+    return;
+  }
+
+  const api = `https://api.mymemory.translated.net/get?q=${encodeURIComponent(text)}&langpair=${writinglanguage}|${convertinglanguage}`;
+
+  fetch(api)
+    .then(response => response.json())
+    .then(data => {
+      outputarea.innerHTML = data.responseData.translatedText;
+      speaktext = data.responseData.translatedText;
+    })
+    .catch(() => {
+      outputarea.innerHTML = "Error occurred while translating.";
     });
-
 }
 
- function speak(){
-  var speech=new SpeechSynthesisUtterance();
-  speech.text=speaktext;
-  speech.lang=convertinglanguage;
+function speak() {
+  if (!speaktext) {
+    outputarea.innerHTML = "Nothing to speak yet.";
+    return;
+  }
+
+  const speech = new SpeechSynthesisUtterance();
+  speech.text = speaktext;
+  speech.lang = convertinglanguage;
   window.speechSynthesis.speak(speech);
- }
-
-
-
-
-
-
-
- 
-
+}
